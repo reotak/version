12d4eb40a1b772baf150749de07345ca7613cefe
高橋耶真人 <ymt@takahashinin-no-MacBook-Pro.local>
2015-09-22 14:00:24 +0900

    complete

--- sample_main.c
+++ sample_main.c
@@ -4,10 +4,15 @@
  * そのため、一度別なマクロを呼び出して値を渡す必要がある
  * このマクロは、その展開を行った後で文字列化するためのマクロ */
 #define __TO_STRING_HELPER(s) #s
+
 /* マクロ名ではなく、マクロの値を文字列化するマクロ */
 #define TO_STRING(var) __TO_STRING_HELPER(var) 
 
 int main(void) {
+#if defined(GIT_BRANCH)
+    printf("GIT_BRANCH: %s\n", TO_STRING(GIT_BRANCH));
+#endif
+
 #if defined(GIT_REVISION)
     printf("GIT_REVISION: %s\n", TO_STRING(GIT_REVISION));
 #else
@@ -15,7 +20,7 @@ int main(void) {
 #endif
 
 #if defined(GIT_MODIFICATIONS)
-    printf("GIT_MODIFICATIONS:\n%s\n", TO_STRING(GIT_MODIFICATIONS));
+    printf("GIT_MODIFICATIONS:%s\n", TO_STRING(GIT_MODIFICATIONS));
 #endif
 
     return 0;
